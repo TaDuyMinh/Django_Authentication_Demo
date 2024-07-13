0eafd095e278603c27ca40d1035ac8ddaa304889
TaMinh <taminhta2003@gmail.com>
2024-07-14 02:59:21 +0700

    fix serializer and register, login ongoing

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
-
 # Create your views here.
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import UserSerializer
+from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+from .models import User
 
 
-@api_view['POST']
+@api_view(['POST'])
 def register_user(request):
     if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
+        serializer = UserSerializer(data=request.data) # take requested data
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['POST'])
+# def login_user(request):
+#     if request.method == 'POST':
+#         username = request.data.get('username')
+#         password = request.data.get('password')
+#
+#         user = None
+#         if '@' in username:
+#             try:
+#                 user = User.objects.get(email=username)
+#             except ObjectDoesNotExist:
+#                 pass
+#
+#         if not user:
+#             user = authenticate(username=username, password=password)
+#
+#         if user:
+#             token, _ = Token.objects
+#
