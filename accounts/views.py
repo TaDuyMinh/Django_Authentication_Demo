7465acfad04ecf0bc49edfe47fc918f1848dd139
TaMinh <taminhta2003@gmail.com>
2024-07-14 03:25:57 +0700

    POST request for login

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 # Create your views here.
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from .serializers import UserSerializer
 from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 from .models import User
 
@@ -21,22 +22,27 @@ def register_user(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @api_view(['POST'])
-# def login_user(request):
-#     if request.method == 'POST':
-#         username = request.data.get('username')
-#         password = request.data.get('password')
-#
-#         user = None
-#         if '@' in username:
-#             try:
-#                 user = User.objects.get(email=username)
-#             except ObjectDoesNotExist:
-#                 pass
-#
-#         if not user:
-#             user = authenticate(username=username, password=password)
-#
-#         if user:
-#             token, _ = Token.objects
-#
+@api_view(['POST'])
+def login_user(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = None
+        if '@' in username:
+            try:
+                user = User.objects.get(email=username)
+            except ObjectDoesNotExist:
+                pass
+
+        if not user:
+            user = authenticate(username=username, password=password)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+
